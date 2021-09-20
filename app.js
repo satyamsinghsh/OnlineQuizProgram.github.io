6baf40daa16611dea7bb186ff16898deacf0e185
satyamsinghsh <91007395+satyamsinghsh@users.noreply.github.com>
2021-09-20 11:35:32 +0530

    Add files via upload
    All pages are linked to each other and it is made by myself which is different from all

---
+++ app.js
@@ -0,0 +1,81 @@
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("bt" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+	
+    element.innerHTML = gameOverHTML;
+
+};
+
+
+// create questions
+var questions = [
+    new Question("What is the Capital of India?", ["New Delhi", "Mumbai","Jaipur", "Kolkata"], "New Delhi"),
+	
+	new Question("Which of the following is the bird?", ["Owl", "Ladybug","Cockroach", "Grasshopper"], "Owl"),
+
+    new Question("National Animal of India?", ["Elephant", "Bengal Tiger","Lion", "Cow"], "Bengal Tiger"),
+	
+	new Question("Which of the following is the smallest bird?", ["Pigeon", "Parrot","Sparrow", "Bee Hummingbird"], "Bee Hummingbird"),
+    
+
+    new Question("National River of India?", ["Yamuna", "Ganga","Brahmputra", "Sindhu"], "Ganga"),
+    
+
+    new Question("Currency of America?", ["Dollar", "Rupee","Euro", "Yen"], "Dollar"),
+	
+	new Question("Nmae the bird?<br><img src='bird.jpg'></img>", ["vulture", "kite","eagle", "falcon"], "kite"),
+	
+	
+	
+        
+
+];
+
+
+var quiz = new Quiz(questions);
+
+
+populate();
+
+
+
+
+
